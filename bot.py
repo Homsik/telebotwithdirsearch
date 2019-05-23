7bd8c2ea224d79f41521c6997e63470c4bde4753
Homsik <faa31.31.31.31@mail.ru>
2019-05-23 06:46:40 +0500

    Bot st. 2

--- bot.py
+++ bot.py
@@ -1,47 +1,29 @@
-# -*- coding: utf-8 -*-
-import requests
-import re
-import functions
+import telebot
 import misc
+import func
+import os
+
+bot = telebot.TeleBot(misc.token)
+global dir_search
+dir_search = False
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет.')
+
+@bot.message_handler(content_types=['text'])
+def get_command(message):
+    if message.text.split()[0] == '/dir': #Исполнение команды
+        dir = message.text.split()[1]
+        dir_search = True #Режим проверки папок
+        dir_data = func.create_list(dir)
+        bot.send_message(message.chat.id, f'Найдено объектов: {dir_data[1]}.\nПуть: {dir_data[2]}/', reply_markup=dir_data[0])
+    if message.text == '[Выйти]' and dir_search: #Выключение
+        dir_search = False
+        bot.send_message(message.chat.id, 'Режим проверки директорий выключен.')
+    if message.text == '[Назад]' and dir_search: #Уровень вверх
+        dir = dir.split('//')[:-1]
+        print(dir)
 
 
-def getUpdates(): #Данные по боту, получение обновлений
-    url = URL + "getUpdates"
-    r = requests.get(url)
-    return r.json()
-
-def getMessage(): #Получение сообщеня
-    data = getUpdates()
-    chat_id = data["result"][-1]["message"]["chat"]["id"]
-    message_text = data["result"][-1]["message"]["text"]
-
-    message = {"chat_id": chat_id, "text": message_text}
-    return message
-
-def sendMessage(chat_id, text='Wait...'): #Отправление сообщения
-    url = URL + f"sendMessage?chat_id={chat_id}&text={text}"
-    requests.get(url)
-
-def main(): #Планируется вебхук
-    update = 0
-    while True:
-        if getUpdates()["result"] != []:
-            if getUpdates()["result"][-1]["update_id"] != update:
-                answer = getMessage()
-                chat_id = answer['chat_id']
-                text = answer['text']
-
-                #Можно перенести в отдельный файл: проверка сообщения и ответ
-                if 'привет' in (recompile.sub('', text)).lower(): #Здаровается
-                    sendMessage(chat_id, "Привет.")
-
-                if "какаятемпературавперми" in (recompile.sub('', text)).lower(): #Запрос на температуру
-                    sendMessage(chat_id, "Температура в Перми равна {}.".format(functions.parse()))
-                update = getUpdates()["result"][-1]["update_id"]
-
-
-token = misc.token #Токен для бота
-URL = f'https://api.telegram.org/bot{token}/'
-recompile = re.compile("[\W]")
-if __name__ == "__main__":
-    main()
+bot.polling()
